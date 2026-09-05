56f462f5f3d276843080a63fef1901d273a7eaa9
🌊 Water Level Monitoring Dashboard <floodpolermutll@gmail.com>
2026-09-05 18:39:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,69 +1,132 @@
 window.WATER_APP_CONFIG = {
-  stations: [
-    {
-      id: "river",
-      name: "สถานีโทรมาตร01",
-      shortName: "สถานีโทรมาตร01",
-      deployed: true,
 
-      latitude: 18.362776,
-      longitude: 99.582243,
+  stations: [
+
+    {
+      id:
+        "river",
+
+      name:
+        "สถานีโทรมาตร01",
+
+      shortName:
+        "สถานีโทรมาตร01",
+
+      deployed:
+        true,
+
+      latitude:
+        18.362776,
+
+      longitude:
+        99.582243,
 
       googleSheetCsv:
         "https://docs.google.com/spreadsheets/d/1TZGZodxs95my8uxbm0w5J6fpPDmIZUAQpwRSKdxPQh4/export?format=csv&gid=994352728",
 
-      unit: "เมตร",
-      warningLevel: 2.5,
+      unit:
+        "เมตร",
 
-      // ระดับสูงสุดของรูปท่อ
-      maxPipeHeight: 3.16,
+      warningLevel:
+        2.5,
 
-      color: "#0B84F3",
-      softColor: "#E4F2FF",
+      /*
+       * ระดับสูงสุดที่แสดงบนรูปท่อ
+       */
+      maxPipeHeight:
+        3.16,
+
+      color:
+        "#0B84F3",
+
+      softColor:
+        "#E4F2FF"
     },
 
-    {
-      id: "flood-point-1",
-      name: "Floodpole 1",
-      shortName: "Floodpole 1",
-      deployed: true,
 
-      latitude: 18.29,
-      longitude: 99.495,
+    {
+      id:
+        "flood-point-1",
+
+      name:
+        "Floodpole 1",
+
+      shortName:
+        "Floodpole 1",
+
+      deployed:
+        true,
+
+      latitude:
+        18.29,
+
+      longitude:
+        99.495,
 
       googleSheetCsv:
         "https://docs.google.com/spreadsheets/d/1TZGZodxs95my8uxbm0w5J6fpPDmIZUAQpwRSKdxPQh4/export?format=csv&gid=615401149",
 
-      unit: "เมตร",
-      warningLevel: 0.8,
+      unit:
+        "เมตร",
 
-      // ระดับสูงสุดของรูปท่อ
-      maxPipeHeight: 3.16,
+      warningLevel:
+        0.8,
 
-      color: "#F97316",
-      softColor: "#FFF0E3",
+      /*
+       * ระดับสูงสุดที่แสดงบนรูปท่อ
+       */
+      maxPipeHeight:
+        3.16,
+
+      color:
+        "#F97316",
+
+      softColor:
+        "#FFF0E3"
     },
 
-    {
-      id: "flood-point-2",
-      name: "Floodpole 2",
-      shortName: "Floodpole 2",
-      deployed: true,
 
-      latitude: 18.295,
-      longitude: 99.497,
+    {
+      id:
+        "flood-point-2",
+
+      name:
+        "Floodpole 2",
+
+      shortName:
+        "Floodpole 2",
+
+      deployed:
+        true,
+
+      latitude:
+        18.295,
+
+      longitude:
+        99.497,
 
       googleSheetCsv:
         "https://docs.google.com/spreadsheets/d/1TZGZodxs95my8uxbm0w5J6fpPDmIZUAQpwRSKdxPQh4/export?format=csv&gid=1870805308",
 
-      unit: "เมตร",
-      warningLevel: 0.8,
+      unit:
+        "เมตร",
 
-      // ระดับสูงสุดของรูปท่อ
-      maxPipeHeight: 3.16,
+      warningLevel:
+        0.8,
 
-      color: "#8B5CF6",
-      softColor: "#F2ECFF",
-    },
-  ],
+      /*
+       * ระดับสูงสุดที่แสดงบนรูปท่อ
+       */
+      maxPipeHeight:
+        3.16,
+
+      color:
+        "#8B5CF6",
+
+      softColor:
+        "#F2ECFF"
+    }
+
+  ]
+
 };
